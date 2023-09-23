32bcd0eb2b64cea9e342fdc4e31757907c8786f4
problemzhang <145827014+problemzhang@users.noreply.github.com>
2023-09-23 14:34:39 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//写代码
+//１.写出主函数（main函数）
+//１００-５００行代码
+// 如何执行？-ｃ语言是从主函数的第一行开始执行的
+// 所以ｃ语言代码中得有main函数-入口
+
+//写个代码，在屏幕上打印你好世界
+//printf-库函数-在屏幕上打印信息的
+// printf的使用，也得打招呼（引用头文件stdio.h）
+//
+
+
+#include <stdio.h>
+int main()
+{
+	printf("hello world\n");
+
+	return 0;
+}
+
+//编译+链接+运行代码
+//快捷键ctrl+f5-运行
